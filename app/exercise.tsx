daaa9b8836fcee1b2d6fd67894bac87cc9a964b1
Ailson Freire <ailson_freire@outlook.com>
2025-03-08 13:13:49 +0100

    Creating exercise page

---
+++ app/exercise.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+// import { View } from "react-native-reanimated/lib/typescript/Animated";
+// import {
+// 	Camera,
+// 	useCameraDevice,
+// 	useCameraPermission,
+// 	useSkiaFrameProcessor,
+// } from "react-native-vision-camera";
+
+export default function Exercise() {
+	// const device = useCameraDevice("front");
+	// const { hasPermission, requestPermission } = useCameraPermission();
+
+	// useEffect(() => {
+	// 	requestPermission().catch((error) => console.log(error));
+	// }, [requestPermission]);
+
+	// const frameProcessor = useSkiaFrameProcessor((frame) => {
+	// 	"worklet";
+	// 	frame.render();
+	// }, []);
+
+	// if (!hasPermission) {
+	// 	return <Text>No permission</Text>;
+	// }
+
+	// if (device == null) {
+	// 	return <Text>No device</Text>;
+	// }
+
+	// const pixelFormat = Platform.OS === "ios" ? "rgb" : "yuv";
+
+	return <View />;
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,30 +1,32 @@
-import { Button, Text, TouchableOpacity, View } from "react-native";
+import { Link } from "expo-router";
+import { Text, TouchableOpacity, View } from "react-native";
 
 export default function Page() {
-  return (
-    //Very ugly and simple UI and style :)
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text style={{ fontSize: 22}}>Wellcome to Movement!</Text>
-      <Text style={{ fontSize: 16}}>Let's do some exercise</Text>
-      <TouchableOpacity 
-        onPress={() => alert("Start")} 
-        style={{
-          borderColor: "black",
-          borderWidth: 1,
-          borderRadius: 5,
-          paddingHorizontal: 40,
-          paddingVertical: 10,
-          margin: 20,
-        }}
-        >
-        <Text style={{ fontSize: 16 }}>Start</Text>
-      </TouchableOpacity>
-    </View>
-  );
+	return (
+		//Very ugly and simple UI and style :)
+		<View
+			style={{
+				flex: 1,
+				justifyContent: "center",
+				alignItems: "center",
+			}}
+		>
+			<Text style={{ fontSize: 22 }}>Wellcome to Movement!</Text>
+			<Text style={{ fontSize: 16 }}>Let's do some exercise</Text>
+			<Link href="/exercise" asChild>
+				<TouchableOpacity
+					style={{
+						borderColor: "black",
+						borderWidth: 1,
+						borderRadius: 5,
+						paddingHorizontal: 40,
+						paddingVertical: 10,
+						margin: 20,
+					}}
+				>
+					<Text style={{ fontSize: 16 }}>Start</Text>
+				</TouchableOpacity>
+			</Link>
+		</View>
+	);
 }

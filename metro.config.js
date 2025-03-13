8998ba14c3a73e939a8d6e553f096a3d2d9b3d23
Ailson Freire <ailson_freire@outlook.com>
2025-03-13 10:08:09 +0100

    setup vision camera config

---
+++ metro.config.js
@@ -0,0 +1,9 @@
+const { getDefaultConfig } = require('expo/metro-config');
+const {
+	wrapWithReanimatedMetroConfig,
+} = require('react-native-reanimated/metro-config');
+
+
+const defaultConfig = getDefaultConfig(__dirname);
+
+module.exports = wrapWithReanimatedMetroConfig(defaultConfig);
